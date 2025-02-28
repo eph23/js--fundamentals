b8c946d04a3703fefa77e586a5cb1a23e802ea60
ephraim <ephraims.818@gmail.com>
2025-03-01 03:57:54 +0600

    JS--Fundamentals - 04. Functions Calling Other Functions

---
+++ 04. Functions Calling Other Functions/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Helper Function
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange`;
+}
+
+console.log(fruitProcessor(2, 3));
