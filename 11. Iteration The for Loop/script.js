6d866a33f8261e446e754130d15b5f693f0413a4
ephraim <ephraims.818@gmail.com>
2025-03-04 01:02:56 +0600

    JS--Fundamentals - 11. Iteration The for Loop

---
+++ 11. Iteration The for Loop/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* 
+In JavaScript, a "for loop" is a control flow statement that allows you to repeatedly execute a block of code. It's designed for situations where you know in advance how many times you want to run the code.
+*/
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`Lifting weights rep ${i} 🏋️‍♂️`);
+}
